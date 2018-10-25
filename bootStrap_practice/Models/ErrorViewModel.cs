42d634a69421b451d0a02cac1a442f46892bc916
smithdaniel1984@outlook.com <smithdaniel1984@outlook.com>
2018-10-25 15:36:08 -0400

    Creating the repo for bootStrap_practice

---
+++ bootStrap_practice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bootStrap_practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
